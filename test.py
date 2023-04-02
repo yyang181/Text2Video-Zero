77a0febaa7636b143687712cc9c014247ffd452a
yyang181 <yyang181@gitee.com>
2023-04-02 14:06:22 +0800

    add new prompt

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ from model import Model
 
 model = Model(device = "cuda", dtype = torch.float16)
 
-prompt = "A horse galloping on a street"
+# prompt = "A horse galloping on a street"
+prompt = "A student running in the rain"
+
 params = {"t0": 44, "t1": 47 , "motion_field_strength_x" : 12, "motion_field_strength_y" : 12, "video_length": 50}
 
 out_path, fps = f"./text2video_{prompt.replace(' ','_')}.mp4", 4
